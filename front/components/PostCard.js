5d85619b283b0287a38da69adb06f5b8a30334a0
bytrustu <bytrustu@gmail.com>
2020-10-26 19:15:02 +0900

    add Hashtag

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -5,6 +5,7 @@ import {RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined, Heart
 import {useSelector} from "react-redux";
 import PostImages from "./PostImages";
 import CommentForm from "./CommentForm";
+import PostCardContent from "./PostCardContent";
 
 const PostCard = ({post}) => {
     const [liked, setLiked] = useState(false);
@@ -44,7 +45,7 @@ const PostCard = ({post}) => {
                 <Card.Meta
                     avartar={<Avatar>{post.User.nickname[0]}</Avatar>}
                     title={post.User.nickname}
-                    description={post.content}
+                    description={<PostCardContent postData={post.content} />}
                 />
             </Card>
             {commentFormOpended && (
