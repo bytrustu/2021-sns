823a9020aba5626df84d9bcc13e20484628d5886
bytrustu <bytrustu@gmail.com>
2020-11-02 18:06:21 +0900

    setting sequelize

--- back/app.js
+++ back/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const app = express();
 const { PORT } = require('./config');
 const postRouter = require('./routes/post')
+const db = require('./models');
 
-const app = express();
+db.sequelize.sync()
+  .then(() => {
+    console.log('db connect success');
+  }).catch(console.error);
+
 app.use('/post', postRouter);
 
 app.listen(PORT, () => {
