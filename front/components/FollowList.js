44bde94a6a45cc821e81879a62d2082c24bd88ec
bytrustu <bytrustu@gmail.com>
2020-11-07 01:52:24 +0900

    apply SWR

--- front/components/FollowList.js
+++ front/components/FollowList.js
@@ -5,7 +5,7 @@ import { StopTwoTone } from '@ant-design/icons';
 import { useDispatch } from 'react-redux';
 import { REMOVE_FOLLOWER_REQUEST, UNFOLLOW_REQUEST } from '../reducers/types';
 
-const FollowList = ({ header, data }) => {
+const FollowList = ({ header, data, onClickMore, loading }) => {
   const dispatch = useDispatch();
   const onCancel = (id) => () => {
     if (header === '팔로잉') {
@@ -27,7 +27,7 @@ const FollowList = ({ header, data }) => {
       grid={{ gutter: 4, xs: 2, md: 3 }}
       size="small"
       header={<div>{header}</div>}
-      loadMore={<div style={{ textAlign: 'center', margin: '10px 0' }}><Button>더 보기</Button></div>}
+      loadMore={<div style={{ textAlign: 'center', margin: '10px 0' }}><Button onClick={onClickMore} loading={loading}>더 보기</Button></div>}
       bordered
       dataSource={data}
       renderItem={(item) => (
@@ -44,6 +44,8 @@ const FollowList = ({ header, data }) => {
 FollowList.propTypes = {
   header: PropTypes.string.isRequired,
   data: PropTypes.array.isRequired,
+  onClickMore: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default FollowList;
--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,34 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Router from 'next/router';
 import axios from 'axios';
+import useSWR from 'swr';
 import { END } from 'redux-saga';
 import HeadComponent from '../components/HeadComponent';
 import AppLayout from '../components/AppLayout';
 import FollowList from '../components/FollowList';
 import NicknameEditForm from '../components/NicknameEditForm';
 import {
-  LOAD_FOLLOWERS_REQUEST,
-  LOAD_FOLLOWINGS_REQUEST,
   LOAD_MY_INFO_REQUEST, LOAD_POST_REQUEST,
 } from '../reducers/types';
 import wrapper from '../store/configureStore';
 
+const fetcher = (url) => axios.get(url, { withCredentials: true })
+  .then((result) => result.data);
+
 const Profile = () => {
   const dispatch = useDispatch();
+  const [followersLimit, setFollowersLimit] = useState(3);
+  const [followingsLimit, setFollowingsLimit] = useState(3);
   const { me } = useSelector((state) => state.user);
+  const { data: followersData, error: followerError } = useSWR(`http://localhost:3065/user/followers?limit=${followersLimit}`, fetcher);
+  const { data: followingsData, error: followingError } = useSWR(`http://localhost:3065/user/followings?limit=${followingsLimit}`, fetcher);
 
   useEffect(() => {
     dispatch({
       type: LOAD_MY_INFO_REQUEST,
-    });
-  }, []);
-  useEffect(() => {
-    dispatch({
-      type: LOAD_FOLLOWERS_REQUEST,
-    });
-    dispatch({
-      type: LOAD_FOLLOWINGS_REQUEST,
     });
   }, []);
   useEffect(() => {
@@ -37,16 +35,29 @@ const Profile = () => {
     }
   }, [me && me.id]);
 
+  const loadMoreFollowings = useCallback(() => {
+    setFollowingsLimit((prev) => prev + 3);
+  }, []);
+
+  const loadMoreFollowers = useCallback(() => {
+    setFollowersLimit((prev) => prev + 3);
+  }, []);
+
   if (!me) {
     return null;
+  }
+
+  if (followerError || followingError) {
+    console.error(followerError || followingError);
+    return <div>팔로잉/팔로워 로딩 중 에러가 발생합니다.</div>;
   }
 
   return (
     <AppLayout>
       <HeadComponent title="내 프로필 | Sns" />
       <NicknameEditForm />
-      <FollowList header="팔로잉" data={me.Followings} />
-      <FollowList header="팔로워" data={me.Followers} />
+      <FollowList header="팔로잉" data={followersData} onClickMore={loadMoreFollowers} loading={!followersData && !followerError} />
+      <FollowList header="팔로워" data={followingsData} onClickMore={loadMoreFollowings} loading={!followingsData && !followingError} />
     </AppLayout>
   );
 };
