b3bf382cfd388d9f0d821db5ec04ef382d51f541
bytrustu <bytrustu@gmail.com>
2021-02-17 00:59:04 +0900

    [FIX] 핸들러 수정

--- front/hooks/useInput.js
+++ front/hooks/useInput.js
@@ -4,7 +4,8 @@ export default (initialValue = null) => {
   const [value, setValue] = useState(initialValue);
   const handler = useCallback((e) => {
     setValue(e.target.value);
-  }, []);
+
+  }, [] );
 
   return [value, handler, setValue];
 };
